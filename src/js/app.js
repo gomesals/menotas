8a2259a5658c5ac561bf487766e2a3af644a9686
Alexandre Silva <contato@silvalexandre.com>
2017-09-11 21:35:54 +0000

    implements delete button funcionalities; updates localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
 /*global Vue,localStorage*/
 (function() {
 	"use strict";
+
+	function updateStorage(data) {
+		localStorage.setItem('notes', JSON.stringify(data));
+	}
 	const notes = new Vue({
 		el: '#notes',
 		data: {
@@ -15,6 +19,13 @@
 				return this.notes.filter(item => {
 					return (item.title.toLowerCase().indexOf(this.search.toLowerCase()) > -1) || (item.content.toLowerCase().indexOf(this.search.toLowerCase()) > -1)
 				});
+			},
+		},
+		methods: {
+			deleteNote: function(note) {
+				const index = this.notes.indexOf(note);
+				this.notes.splice(index, 1);
+				updateStorage(this.notes);
 			},
 		}
 	});
