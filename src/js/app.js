6fff2b9099bd3e8ca5c039473d020aa626230abd
Alexandre Silva <contato@silvalexandre.com>
2017-09-11 21:52:05 +0000

    implements form and save to localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,14 @@
 (function() {
 	"use strict";
 
-	function updateStorage(data) {
-		localStorage.setItem('notes', JSON.stringify(data));
+	function updateStorage(notes) {
+		localStorage.setItem('notes', JSON.stringify(notes));
+	}
+
+	function addItem(note) {
+		let notes = JSON.parse(localStorage.getItem('notes'));
+		notes = notes.concat(note);
+		updateStorage(notes);
 	}
 	const notes = new Vue({
 		el: '#notes',
@@ -29,5 +35,29 @@
 			},
 		}
 	});
-
+	const editor = new Vue({
+		el: '#editor',
+		data: {
+			title: '',
+			content: '',
+			saved: false,
+		},
+		methods: {
+			save: function() {
+				if (this.title === '' && this.content === '') {
+					return false;
+				}
+				addItem({
+					title: this.title,
+					content: this.content
+				});
+				this.saved = true;
+				this.title = '';
+				this.content = '';
+				setTimeout(() => {
+					this.saved = false;
+				}, 3500);
+			}
+		}
+	})
 })();
